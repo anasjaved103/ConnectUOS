989d6847ed0e25f15618d8eff364746316e2f694
anasjaved103 <energaticmrs@gmail.com>
2024-12-10 22:36:30 +0500

    Update module1updated.cpp

--- module1updated.cpp
+++ module1updated.cpp
@@ -1,4 +1,4 @@
-                       // By Muhammad Anas bin Javed.
+                       // By Muhammad Anas  Javed.
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -6,7 +6,8 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     const int SIZE = 2000;
     string names[SIZE];
     int counts[SIZE] = {0};
@@ -15,31 +16,48 @@ int main() {
     int numNames = 0;
 
     ifstream file("Friends Dataset.csv");
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         cout << "Error: Unable to open file." << endl;
         return 1;
     }
 
     string line, name;
-    while (getline(file, line)) {
+    while (getline(file, line))
+      {
         stringstream ss(line);
-        while (getline(ss, name, ',')) {
+        while (getline(ss, name, ','))
+          
+        
+        {
             names[numNames] = name;
             numNames++;
+       
+        
         }
     }
     file.close();
 
-    for (int i = 0; i < numNames; i++) {
-        for (int j = 0; j < numNames; j++) {
+    for (int i = 0; i < numNames; i++)
+      
+    {
+        for (int j = 0; j < numNames; j++)
+          
+        {
             if (names[i] == names[j]) {
                 counts[i]++;
             }
-        }
+       
+           }
     }
 
-    for (int i = 0; i < numNames; i++) {
-        if (counts[i] > maxCount) {
+    for (int i = 0; i < numNames; i++)
+      
+    {
+        if (counts[i] > maxCount)
+        
+        
+        {
             maxCount = counts[i];
             mostPopularName = names[i];
         }
